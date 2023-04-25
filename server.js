bb97158f4a6f1bc8b4cafa01665c2e22881f9380
GitDidrik <lindberg.didrik@gmail.com>
2023-04-24 19:22:21 -0700

    attempting to fix  undef. title error in post req

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 // Require the JSON file and assign it to a variable called noteData
 const noteData = require('./db/db.json');
+const fs = require('fs');
 
 
 // Import the Express.js library and create an instance of it
@@ -32,55 +33,41 @@ app.get('/api/notes', (req, res) => res.json(noteData));
 // Route to handle POST requests to /api/notes endpoint and sends the noteData object from the db.json file as a response
 
 // get the data from the request body, add it to noteData, then return the new noteData object as a response
+app.post('/api/notes', (req, res) => {
 
-// route to add a new note and add it to the json file
-app.post("/api/notes", async function (req, res) {
-    const currentNotes = await noteData.readNotes();
-    let newNote = {
-      id: uuid(),
-      title: req.body.title,
-      text: req.body.text,
-    };
-  
-    await noteData.addNote([...currentNotes, newNote]);
-  
-    return res.send(newNote);
-  });
-// app.post('/api/notes', (req, res) => {
-
-//      // Set the Content-Type header to application/json
-//      res.setHeader('Content-Type', 'application/json');
+     // Set the Content-Type header to application/json
+     res.setHeader('Content-Type', 'application/json');
      
-//     // Destructuring assignment for the items in req.body
-//     // const { title, text } = req.body;
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
 
-//     // If all the required properties are present
-//     if (title && text) {
-//         // Variable for the object we will save
-//         const newNote = {
-//             title,
-//             text,
-//         };
+    // If all the required properties are present
+    if (title && text) {
+        // Variable for the object we will save
+        const newNote = {
+            title,
+            text,
+        };
 
-//         // Obtain a unique id for the note
-//         const uniqueId = noteData.length + 1;
-//         // Add the unique id to the note object
-//         newNote.id = uniqueId;
-//         // Push the note object to the noteData array
-//         noteData.push(newNote);
+        // Obtain a unique id for the note
+        const uniqueId = noteData.length + 1;
+        // Add the unique id to the note object
+        newNote.id = uniqueId;
+        // Push the note object to the noteData array
+        noteData.push(newNote);
 
-//         // Write the noteData array to the db.json file
-//         fs.writeFileSync(
-//             path.join(__dirname, './db/db.json'),
-//             JSON.stringify(noteData, null, 2)
-//         );
+        // Write the noteData array to the db.json file
+        fs.writeFileSync(
+            path.join(__dirname, './db/db.json'),
+            JSON.stringify(noteData, null, 2)
+        );
 
-//         // Return the new note as response
-//         res.json(newNote);
-//     } else {
-//         res.json('Error in posting note');
-//     }
-// });
+        // Return the new note as response
+        res.json(newNote);
+    } else {
+        res.json('Error in posting note');
+    }
+});
 
 
 
