31072e45236051d9aa2b0563f1d7332f301f3ca8
GitDidrik <lindberg.didrik@gmail.com>
2023-04-24 17:40:41 -0700

    commenting

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ const express = require('express');
 const path = require('path');
 
 
-// create express app
+// Import the Express.js library and create an instance of it
 const app = express();
+// Set the port number to 300
 const PORT = 3002;
 
-// 
+// Serve static files in the 'public' folder
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+// Define a route for the home page and send a response
+// app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
+
+
+
+// Start the server and listen for incoming connections on the specified port
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
