15c6751abd26daa0dd68643231e31d69bdb5d8f7
GitDidrik <lindberg.didrik@gmail.com>
2023-04-24 18:16:40 -0700

    displays notes in db.json on /notes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 // Require the JSON file and assign it to a variable called noteData
 const noteData = require('./db/db.json');
 
+
 // Import the Express.js library and create an instance of it
 const app = express();
 // Set the port number to 300
@@ -13,13 +14,19 @@ const PORT = 3002;
 app.use(express.static('public'));
 
 
+// Route to handle GET requests to /notes and sends the notes.html file as a response
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+  });
+
+// This route handles GET requests to the root URL and sends a message with a link to /api/notes as a response
 app.get('/', (req, res) => res.send('Visit http://localhost:3001/api/notes'));
 
+// Route to handle GET requests to /api/notes endpoint and sends the noteData object from the db.json file as a response
 app.get('/api/notes', (req, res) => res.json(noteData));
 
 
 // Define a route for the home page and send a response
-// app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
 
 
