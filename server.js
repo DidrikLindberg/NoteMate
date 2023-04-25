a7ab331b764877c9fd6d946683fe80092a69cdb1
GitDidrik <lindberg.didrik@gmail.com>
2023-04-24 19:09:02 -0700

    installed uuid package

--- server.js
+++ server.js
@@ -32,37 +32,55 @@ app.get('/api/notes', (req, res) => res.json(noteData));
 // Route to handle POST requests to /api/notes endpoint and sends the noteData object from the db.json file as a response
 
 // get the data from the request body, add it to noteData, then return the new noteData object as a response
-app.post('/api/notes', (req, res) => {
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
 
-    // If all the required properties are present
-    if (title && text) {
-        // Variable for the object we will save
-        const newNote = {
-            title,
-            text,
-        };
+// route to add a new note and add it to the json file
+app.post("/api/notes", async function (req, res) {
+    const currentNotes = await noteData.readNotes();
+    let newNote = {
+      id: uuid(),
+      title: req.body.title,
+      text: req.body.text,
+    };
+  
+    await noteData.addNote([...currentNotes, newNote]);
+  
+    return res.send(newNote);
+  });
+// app.post('/api/notes', (req, res) => {
 
-        // Obtain a unique id for the note
-        const uniqueId = noteData.length + 1;
-        // Add the unique id to the note object
-        newNote.id = uniqueId;
-        // Push the note object to the noteData array
-        noteData.push(newNote);
+//      // Set the Content-Type header to application/json
+//      res.setHeader('Content-Type', 'application/json');
+     
+//     // Destructuring assignment for the items in req.body
+//     // const { title, text } = req.body;
 
-        // Write the noteData array to the db.json file
-        fs.writeFileSync(
-            path.join(__dirname, './db/db.json'),
-            JSON.stringify(noteData, null, 2)
-        );
+//     // If all the required properties are present
+//     if (title && text) {
+//         // Variable for the object we will save
+//         const newNote = {
+//             title,
+//             text,
+//         };
 
-        // Return the new note as response
-        res.json(newNote);
-    } else {
-        res.json('Error in posting note');
-    }
-});
+//         // Obtain a unique id for the note
+//         const uniqueId = noteData.length + 1;
+//         // Add the unique id to the note object
+//         newNote.id = uniqueId;
+//         // Push the note object to the noteData array
+//         noteData.push(newNote);
+
+//         // Write the noteData array to the db.json file
+//         fs.writeFileSync(
+//             path.join(__dirname, './db/db.json'),
+//             JSON.stringify(noteData, null, 2)
+//         );
+
+//         // Return the new note as response
+//         res.json(newNote);
+//     } else {
+//         res.json('Error in posting note');
+//     }
+// });
 
 
 
