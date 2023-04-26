0dda93540ac266baf90bb50dd9ac3fce9a48ce95
GitDidrik <lindberg.didrik@gmail.com>
2023-04-25 19:32:26 -0700

    miniApp added to routes>index.html

--- server.js
+++ server.js
@@ -4,15 +4,23 @@ const path = require('path');
 // Require the JSON file and assign it to a variable called noteData
 const noteData = require('./db/db.json');
 const fs = require('fs');
+const api = require('./routes/index.js');
+// const apiRoutes = require('./routes/apiRoutes.js');
+
+
 
 
 // Import the Express.js library and create an instance of it
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+// Tell the app to use the routes in apiRoutes.js
+app.use('/api', api);
 // Set the port number to 3002
 const PORT = 3002;
 
 // Middleware to parse JSON data in request body
 app.use(express.json());
+app.use("/api", require("./routes/apiRoutes"));
 
 // Serve static files in the 'public' folder
 app.use(express.static('public'));
@@ -43,33 +51,33 @@ app.get('/api/notes', (req, res) => res.json(noteData));
 // // Route to handle POST requests to /api/notes endpoint and sends the noteData object from the db.json file as a response
 
 // // get the data from the request body, add it to noteData, then return the new noteData object as a response
-app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
+// app.post('/api/notes', (req, res) => {
+//     console.info(`${req.method} request received to add a note`);
 
 
-    let response;
+//     let response;
     
-    //check if there is data in the request body
-    if (req.body) {
-        response = {
-            status: 'success',
-            data: req.body
-        };
+//     //check if there is data in the request body
+//     if (req.body) {
+//         response = {
+//             status: 'success',
+//             data: req.body
+//         };
 
-        //add a new note to the array
-        noteData.push(req.body);
+//         //add a new note to the array
+//         noteData.push(req.body);
 
-        //write the noteData array to the db.json file
-        fs.writeFileSync(
-            path.join(__dirname, './db/db.json'),
-            JSON.stringify(noteData, null, 2)
-        );
-        res.json(response);
-    } else {
-        res.json('Error in posting note');
-    }
+//         //write the noteData array to the db.json file
+//         fs.writeFileSync(
+//             path.join(__dirname, './db/db.json'),
+//             JSON.stringify(noteData, null, 2)
+//         );
+//         res.json(response);
+//     } else {
+//         res.json('Error in posting note');
+//     }
 
-});
+// });
 
 
 
